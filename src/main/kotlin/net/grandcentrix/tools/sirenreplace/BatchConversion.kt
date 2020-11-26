93aca6513a04d0505b74b4c39fc8f83067b6eff0
Lukasz Kalnik <lukasz.kalnik@grandcentrix.net>
2020-11-26 18:03:44 +0100

    Pretty print JSON and provide run statistics

--- src/main/kotlin/net/grandcentrix/tools/sirenreplace/BatchConversion.kt
+++ src/main/kotlin/net/grandcentrix/tools/sirenreplace/BatchConversion.kt
@@ -1,11 +1,14 @@
 package net.grandcentrix.tools.sirenreplace
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import java.io.File
+import kotlin.system.measureTimeMillis
 
+@ExperimentalSerializationApi
 fun main(args: Array<String>) {
     batchSirenMoveObject(
         path = args[0],
@@ -16,6 +19,7 @@ fun main(args: Array<String>) {
     )
 }
 
+@ExperimentalSerializationApi
 fun batchSirenMoveObject(
     path: String,
     sourceTopLevelKey: String,
@@ -23,27 +27,40 @@ fun batchSirenMoveObject(
     sourceObjectKey: String,
     targetObjectKey: String
 ) {
-    val files = File(path).listFiles() ?: return
-    files.filter { it.path.endsWith(".json") }
-        .forEach { file ->
-            val reader = file.bufferedReader()
-            val fileContents = reader.use { it.readText() }
-            val inputJsonObject = Json.decodeFromString<JsonObject>(fileContents)
+    var openFileCounter = 0
+    var modifiedFileCounter = 0
 
-            // Convert the file contents
-            val modifiedJsonObject = sirenMoveObject(
-                inputJsonObject,
-                sourceTopLevelKey,
-                sourceClass,
-                sourceObjectKey,
-                targetObjectKey
-            )
+    val encodingFormat = Json {
+        prettyPrint = true
+        prettyPrintIndent = "  "
+    }
 
-            // If the modifiedJsonObject was null then there was nothing to convert -> skip it
-            if (modifiedJsonObject != null) {
-                val modifiedFileContents = Json.encodeToString(modifiedJsonObject)
-                val writer = file.bufferedWriter()
-                writer.use { it.write(modifiedFileContents) }
+    val elapsedTime = measureTimeMillis {
+        val files = File(path).listFiles() ?: return
+        files.filter { it.path.endsWith(".json") }
+            .forEach { file ->
+                ++openFileCounter
+                val reader = file.bufferedReader()
+                val fileContents = reader.use { it.readText() }
+                val inputJsonObject = Json.decodeFromString<JsonObject>(fileContents)
+
+                // Convert the file contents
+                val modifiedJsonObject = sirenMoveObject(
+                    inputJsonObject,
+                    sourceTopLevelKey,
+                    sourceClass,
+                    sourceObjectKey,
+                    targetObjectKey
+                )
+
+                // If the modifiedJsonObject was null then there was nothing to convert -> skip it
+                if (modifiedJsonObject != null) {
+                    ++modifiedFileCounter
+                    val modifiedFileContents = encodingFormat.encodeToString(modifiedJsonObject)
+                    val writer = file.bufferedWriter()
+                    writer.use { it.write(modifiedFileContents) }
+                }
             }
-        }
+    }
+    println("SirenReplace modified $modifiedFileCounter files out of $openFileCounter in $elapsedTime ms")
 }
